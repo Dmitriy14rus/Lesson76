e086c4f18158e58895cd536a38b9000ed6473dec
Dmitriy14rus <129680732+Dmitriy14rus@users.noreply.github.com>
2023-07-29 22:03:04 +0900

    Add files via upload

---
+++ docs/main.js
@@ -0,0 +1,18 @@
+const userName = 'Дмитрий'
+const userSurname = 'Жданкин'
+let age = 28
+let work = 'безработный'
+let hobbies = 'вкусно поесть,крепко и долго поспать=)'
+
+console.log('Собеседник1->Как вас зовут?')
+console.log('Собеседник2->Меня зовут',userName)
+console.log('Собеседник1->Ваша фамилия?')
+console.log('Собеседник2->Моя фамилия',userSurname)
+console.log('Собеседник1->Ваш возраст?')
+console.log('Собеседник2->Мне',age,'лет')
+console.log('Собеседник1->Кем вы работаете?')
+console.log('Собеседник2->Я пока,что',work)
+console.log('Собеседник1->Ваше увлечение?')
+console.log('Собеседник2->Я профессионально могу',hobbies,)
+
+
